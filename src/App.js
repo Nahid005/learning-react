8bb693b2ba10345f085405f7fc7037e4b1fe336a
Nahid005 <nahidhassan9258@gmail.com>
2022-09-17 00:17:36 +0600

    toggle add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import ClassComponent from './component/classComponent/ClassComponent'
-import FanctionalComponent from './component/functionalComponent/FunctionalComponent'
-import StateHandlingClass from './component/classComponent/StateHandlingClass'
-import StateHandlingFunc from './component/functionalComponent/StateHandlingFunc'
-import PropsClass from './component/classComponent/PropsClass'
-import PropsFunc from './component/functionalComponent/PropsFunc'
-import FormHandleClass from './component/classComponent/FormHandleClass'
-import FormHandleFunc from './component/functionalComponent/formHandleFunc'
-import Home from './component/todoApp/Home'
-import UseEffect from './component/functionalComponent/UseEffect'
+// import ClassComponent from './component/classComponent/ClassComponent'
+// import FanctionalComponent from './component/functionalComponent/FunctionalComponent'
+// import StateHandlingClass from './component/classComponent/StateHandlingClass'
+// import StateHandlingFunc from './component/functionalComponent/StateHandlingFunc'
+// import PropsClass from './component/classComponent/PropsClass'
+// import PropsFunc from './component/functionalComponent/PropsFunc'
+// import FormHandleClass from './component/classComponent/FormHandleClass'
+// import FormHandleFunc from './component/functionalComponent/formHandleFunc'
+// import Home from './component/todoApp/Home'
+// import UseEffect from './component/functionalComponent/UseEffect'
+import Toggle from './component/toggle/Toggle'
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent />
+      {/* <ClassComponent />
       <FanctionalComponent />
       <StateHandlingClass />
       <StateHandlingFunc />
@@ -22,7 +23,8 @@ function App() {
       <FormHandleClass />
       <FormHandleFunc />
       <UseEffect />
-      <Home />
+      <Home /> */}
+      <Toggle />
     </div>
   );
 }
