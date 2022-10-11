d0156bb62358bcb8e77cb8d055cc2fae8db7e444
Nahid005 <nahidhassan9258@gmail.com>
2022-10-11 23:10:17 +0600

    use effect add

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -2,29 +2,36 @@ import React, {useEffect, useState} from 'react'
 
 const Home = () => {
 
-    const [user, setUser] = useState()
+    const [datas, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
-    useEffect(()=> {
-        const url = 'https://jsonplaceholder.typicode.com/users'
-        const fetchData = async () => {
-            try{
-                const response = await fetch(url)
-                const json = await response.json()
-                console.log(json)
-            }
-            catch(error) {
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                setData(data)
+                setIsLoading(false)
+            })
+            .catch((error) => {
                 console.log(error)
-            }
-        }
-
-        setUser(fetchData())
-    }, [])
-
-
+                setIsLoading(false)
+            })
+    },[])
 
     return(
         <div>
-            
+            {
+                datas.map((data) => {
+                    return (
+                        <div>
+                            <p> {isLoading && <p> loading   </p> } </p>
+                            <p> {data.name} </p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
